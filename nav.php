bbed6c762c6655f472f7b3dffc36d3c02135b3de
IlhanRomeo <92511879+IlhanRomeo@users.noreply.github.com>
2021-10-14 21:06:42 +0800

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<h1>Police Emergency Service System</h1>
+<nav>
+ <a href="logcall.php">Log Call</a>
+ <a href="Update.php">Update</a>
+ <a href="#">Report</a>
+ <a href="#">History</a>
+ </nav>
+<body>
+</body>
+</html>
